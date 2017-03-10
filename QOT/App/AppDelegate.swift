3841514daf627fd8b7fe24c20141ff53bc7304a4
Sam Wyndham <samwyndham@Sams-MacBook-Pro.local>
2017-03-10 11:33:05 +0100

    Add mock content data

--- QOT/App/AppDelegate.swift
+++ QOT/App/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        setupRealmWithMockData() // FIXME: Remove when not needed
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         appCoordinator.start()
         return true
---
+++ QOT/App/MockData.swift
@@ -0,0 +1,91 @@
+//
+//  MockData.swift
+//  QOT
+//
+//  Created by Sam Wyndham on 10/03/2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+// FIXME: Remove when no longer needed
+
+/// Deletes all data in default realm and fills with mock data.
+func setupRealmWithMockData() {
+    do {
+        let realm = try Realm()
+        try realm.write {
+            realm.deleteAll()
+            realm.add(mockContentCategories())
+        }
+    } catch let error {
+        fatalError("Realm error: \(error)")
+    }
+}
+
+private var mockID = 0
+
+private func newMockID() -> Int {
+    let id = mockID
+    mockID += 1
+    return id
+}
+
+private func mockContentItems() -> [ContentItem] {
+    let datas: [ContentItem.Data] = [.text("some text"), .video(URL(string: "a_url")!)]
+    var items: [ContentItem] = []
+    for _ in 0...Int.random(between: 3, and: 10) {
+        let item = ContentItem(id: newMockID(), title: "", status: 0, data: datas.randomItem())
+        items.append(item)
+    }
+    return items
+}
+
+private func mockContent() -> [Content] {
+    let possibleTitles = [
+        "Performance mindset defined",
+        "Identify Mindset Killers",
+        "Eliminate Drama",
+        "From Low To High Performance",
+        "Optimal Performance State",
+        "Mindset Shift",
+        "Building on High Performance",
+        "Reframe Thoughts",
+        "Visulize Success",
+        "Control the Controllable",
+        "Mental Rehearsal",
+        "Set Intentions",
+        "Performance Mindset Defined"
+    ]
+    
+    let titles = possibleTitles.prefix(upTo: Int.random(between: 3, and: possibleTitles.count + 1))
+    return titles.map { (title) -> Content in
+        let content = Content(id: newMockID(), name: "some name", title: title)
+        content.items.append(objectsIn: mockContentItems())
+        return content
+    }
+}
+
+private func mockContentCategories() -> [ContentCategory] {
+    let categoryTitles = ["Mindset", "Recovery", "Foundation", "Nutrition", "New", "Movement", "Habituation"]
+    return categoryTitles.map { (title) -> ContentCategory in
+        let category = ContentCategory(id: newMockID(), name: "a name", title: title)
+        category.contents.append(objectsIn: mockContent())
+        return category
+    }
+}
+
+private extension Array {
+    func randomItem() -> Element {
+        return self[Int(arc4random_uniform(UInt32(self.count)))]
+    }
+}
+
+private extension Int {
+    /// Returns random number from `min` to `max` exclusive.
+    static func random(between min: Int, and max: Int) -> Int {
+        assert(min < max)
+        return Int(arc4random_uniform(UInt32(max) - UInt32(min))) + min
+    }
+}
