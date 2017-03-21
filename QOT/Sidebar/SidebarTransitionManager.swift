d60b584c2236b33b9f9399d67088b3fd0c706de6
karmic <karmic@karmic.local>
2017-03-21 10:26:00 +0100

    feature/IT-183: Setup cus

---
+++ QOT/Sidebar/SidebarTransitionManager.swift
@@ -0,0 +1,37 @@
+//
+//  SidebarTransitionManager.swift
+//  QOT
+//
+//  Created by karmic on 20/03/2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+class SidebarTransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+    
+    // MARK: UIViewControllerAnimatedTransitioning protocol methods
+    
+    // animate a change from one viewcontroller to another
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        //TODO
+    }
+    
+    // return how many seconds the transiton animation will take
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    // MARK: UIViewControllerTransitioningDelegate protocol methods
+    
+    // return the animataor when presenting a viewcontroller
+    // remmeber that an animator (or animation controller) is any object that aheres to the UIViewControllerAnimatedTransitioning protocol
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
+    // return the animator used when dismissing from a viewcontroller
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+}
---
+++ QOT/Sidebar/SidebarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarViewController.swift
+//  QOT
+//
+//  Created by karmic on 20/03/2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol SidebarDelegate {
+    func openSideMenu()
+}
+
+final class SidebarViewController: UIViewController {
+    
+}
