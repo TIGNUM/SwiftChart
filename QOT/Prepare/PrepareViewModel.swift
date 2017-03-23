49668370e99463ca2950d122cd4d1f136fc08757
karmicDev <m.karbe@tignum.com>
2017-03-23 17:25:41 +0100

    feature/IT-193: Add some mockData

--- QOT/Prepare/PrepareViewModel.swift
+++ QOT/Prepare/PrepareViewModel.swift
@@ -20,32 +20,61 @@ final class PrepareChatBotViewModel {
 
     // MARK: - Properties
 
-    private let categories: Results<PrepareCategory>
+//    private let categories: Results<PrepareCategory>
+    private var messages = mockData()
     private var token: NotificationToken?
+    let updates = PublishSubject<CollectionUpdate, NoError>()
 
     var categoryCount: Index {
-        return categories.count
+        return messages.count
     }
 
     // MARK: - Life Cycle
 
-    init(categories: Results<PrepareCategory>) {
-        self.categories = categories
-        self.token = categories.addNotificationBlock { (change) in
-            switch change {
-            case .error(let error):
-                assertionFailure("An error occurred: \(error)")
-            case .initial(_):
-                QOTLog(Verbose.Database.Prepare, "Initial run of the query has completed.")
-            case .update(_, let deletions, let insertions, let modifications):
-                QOTLog(Verbose.Database.Prepare, "Update deletions: \(deletions), insertions: \(insertions), modifications: \(modifications)")
-            }
-        }
+//    init(categories: Results<PrepareCategory>) {
+//        self.categories = categories
+//        self.token = categories.addNotificationBlock { (change) in
+//            switch change {
+//            case .error(let error):
+//                assertionFailure("An error occurred: \(error)")
+//            case .initial(_):
+//                QOTLog(Verbose.Database.Prepare, "Initial run of the query has completed.")
+//            case .update(_, let deletions, let insertions, let modifications):
+//                QOTLog(Verbose.Database.Prepare, "Update deletions: \(deletions), insertions: \(insertions), modifications: \(modifications)")
+//            }
+//        }
+//    }
+
+    init() {
+
     }
 
     // MARK: - Helpers
 
-    func category(at index: Index) -> PrepareCategory {
+    func category(at index: Index) -> ChatMessage {
         return categories[index] as PrepareCategory
     }
 }
+
+protocol ChatMessage {
+    var type: ChatMessageType { get }
+}
+
+enum ChatMessageType {
+    case instructionTyping
+    case instruction(text: String, delivered: Date)
+    case options(title: String, options: [Option])
+
+    enum Option {
+        case navigation
+        case input
+    }
+}
+
+private func mockData() -> [ChatMessage] {
+    return [
+        ChatMessageType.instruction(text: "Hi Loui", delivered: <#T##Date#>)
+    ]
+}
+
+// add 
