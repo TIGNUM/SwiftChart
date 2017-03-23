611a7b3304a6febea77a7c58335668e954d59830
karmicDev <m.karbe@tignum.com>
2017-03-23 09:22:33 +0100

    feature/IT-193: Clean up AppDelegate.

--- QOT/App/AppDelegate.swift
+++ QOT/App/AppDelegate.swift
@@ -10,11 +10,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+
     private lazy var appCoordinator: AppCoordinator = {
         return AppCoordinator(window: self.window!)
     }()
     
     var window: UIWindow?
+
+    // MARK: - Life Cycle
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {        
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -27,14 +32,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    
-    func printAppLocation() {
+}
+
+extension AppDelegate {
+
+    fileprivate func printAppLocation() {
         let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let url = documents.deletingLastPathComponent()
         print("App location: \(url)")
     }
-    
-    private func printAvailableFonts() {
+
+    fileprivate func printAvailableFonts() {
         for family: String in UIFont.familyNames {
             print("\(family)")
             for names: String in UIFont.fontNames(forFamilyName: family) {
