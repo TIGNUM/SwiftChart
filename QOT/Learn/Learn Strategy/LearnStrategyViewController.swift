366ee92de538153b2e98786f83a9f37769c345a6
karmicDev <m.karbe@tignum.com>
2017-03-29 10:30:40 +0200

    feature/IT-252: Add LearnStrategyViewController

---
+++ QOT/Learn/Learn Strategy/LearnStrategyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LearnStrategyViewController.swift
+//  QOT
+//
+//  Created by karmic on 29/03/2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+protocol LearnStrategyViewDelegate: class {
+    func didTapClose(in viewController: PrepareContentViewController)
+    func didTapShare(in viewController: PrepareContentViewController)
+    func didTapVideo(with localID: String, from view: UIView, in viewController: PrepareContentViewController)
+}
+
+final class LearnStrategyViewController: UITableViewController {
+
+}
