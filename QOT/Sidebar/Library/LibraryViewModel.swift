a1c3e04279c3556f9a9f1bb7d5315979399e1cff
karmicDev <m.karbe@tignum.com>
2017-03-30 15:02:47 +0200

    feature/IT-267: Update LibraryViewModel

--- QOT/Sidebar/Library/LibraryViewModel.swift
+++ QOT/Sidebar/Library/LibraryViewModel.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import ReactiveKit
+
+final class LibraryViewModel {
+
+    private let sections = [LibrarySection]()
+    let updates = PublishSubject<CollectionUpdate, NoError>()
+
+    var sectionCount: Int {
+        return sections.count
+    }
+
+    func numberOfItemsInSection(in section: Int) -> Int {
+        return items(in: section).count
+    }
+
+    func item(at indexPath: IndexPath) -> LearnStrategyItem {
+        return items(in: indexPath.section)[indexPath.row]
+    }
+
+    private func items(in section: Int) -> [LearnStrategyItem] {
+        return []
+    }
+}
+
+private enum LibrarySection {
+    case lastPostItems(LibraryItem)
+    case categoryItems([LibraryItem])
+}
+
+enum LibraryItem {
+    case header(localID: String, title: String)
+    case collection(localID: String, headline: String, text: String, media: MediaType)
+
+    enum MediaType {
+        case audio(localID: String, placeholderURL: URL, title: String)
+        case video(localID: String, placeholderURL: URL, title: String)
+    }
+}
+
+private func mock
