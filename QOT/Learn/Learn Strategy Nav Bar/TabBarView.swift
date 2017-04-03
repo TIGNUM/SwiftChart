b196b8b2f1001a2f13237765cb3b2252568e5383
tignum <tignum@tignums-Mac-mini.local>
2017-04-03 15:08:27 +0200

    added tabed view .. added animation and color variation on click..

--- QOT/Learn/Learn Strategy Nav Bar/TabBarView.swift
+++ QOT/Learn/Learn Strategy Nav Bar/TabBarView.swift
@@ -10,7 +10,18 @@ import UIKit
 import Anchorage
 class TabBarView: UIView {
     
-    var titles: [String] = ["Full", "Short"]
+    var titles: [String] = ["Full", "Short", "New"]
+    var selectedIndex: Int = 0
+    fileprivate weak var indicatorViewLeadingConstraint: NSLayoutConstraint?
+    fileprivate weak var indicatorViewWidthConstraint: NSLayoutConstraint?
+    
+    struct Constants {
+        static let animationDuration: TimeInterval = 0.3
+        static let selectedButtonColor: UIColor = .white
+        static let deselectedButtonColor: UIColor = UIColor.white.withAlphaComponent(0.4)
+        static let stackViewHorizontalPadding: CGFloat = 16
+        static let indicatorViewExtendedWidth: CGFloat = 16
+    }
     
     fileprivate lazy var stackView: UIStackView = {
         let view = UIStackView()
@@ -26,22 +37,74 @@ class TabBarView: UIView {
             button.setTitle(title, for: .normal)
             button.titleLabel?.font = Font.TabBarController.buttonTitle
             button.backgroundColor = .clear
-            button.setTitleColor(UIColor.white.withAlphaComponent(0.4), for: .normal)
+            button.setTitleColor(Constants.deselectedButtonColor, for: .normal)
             button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
             button.tag = index
             return button
         }
     }()
     
+    fileprivate lazy var indicatorView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
     override func awakeFromNib() {
-        self.backgroundColor = .red
+        self.backgroundColor = .black
        print("hello")
         setupHierachy()
         setupLayout()
+        syncIndicatorView(animated: true)
+        syncButtonColors(animated: true)
     }
     
-    @objc private func buttonPressed(_:UIButton) {
-       print(buttons.count)
+    @objc private func buttonPressed(_ button: UIButton) {
+       
+        guard button.tag != selectedIndex else {
+            return
+        }
+        selectedIndex = button.tag
+        syncIndicatorView(animated: true)
+        syncButtonColors(animated: true)
+    }
+    
+    func syncButtonColors(animated: Bool) {
+        let getColor: (UIButton, Int) -> UIColor = { (button, index) in
+            index == button.tag ? Constants.selectedButtonColor : Constants.deselectedButtonColor
+        }
+        
+        if animated {
+            for button in buttons {
+                let color = getColor(button, selectedIndex)
+                let transition = UIViewAnimationOptions.transitionCrossDissolve
+                let duration = Constants.animationDuration
+                UIView.transition(with: button, duration: duration, options: transition, animations: {
+                    button.setTitleColor(color, for: .normal)
+                }, completion: nil)
+            }
+        } else {
+            for button in buttons {
+                button.setTitleColor(getColor(button, selectedIndex), for: .normal)
+            }
+        }
+    }
+    
+    func syncIndicatorView(animated: Bool) {
+        let button = buttons[selectedIndex]
+        let width = button.intrinsicContentSize.width + Constants.indicatorViewExtendedWidth
+        
+        indicatorViewWidthConstraint?.constant = width
+        indicatorViewLeadingConstraint?.constant = button.center.x - (width / 2) + Constants.stackViewHorizontalPadding
+        
+        if animated {
+            let transition = UIViewAnimationOptions.curveEaseInOut
+            UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: transition, animations: {
+                self.layoutIfNeeded()
+            }, completion: nil)
+        } else {
+            layoutIfNeeded()
+        }
     }
 }
 
@@ -49,6 +112,8 @@ private extension TabBarView {
     
     func setupHierachy() {
         addSubview(stackView)
+         buttons.forEach { stackView.addArrangedSubview($0) }
+        addSubview(indicatorView)
     }
     
     func setupLayout() {
@@ -57,6 +122,12 @@ private extension TabBarView {
         stackView.leftAnchor == leftAnchor
         stackView.rightAnchor == rightAnchor
         
-        heightAnchor == 64
+        indicatorView.bottomAnchor == bottomAnchor
+        indicatorView.heightAnchor == 1
+        indicatorViewLeadingConstraint = indicatorView.leadingAnchor == leadingAnchor
+        indicatorViewWidthConstraint = indicatorView.widthAnchor == 0
+        
+        layoutIfNeeded()
+        
     }
 }
