506a8c33c91de94c811d86421179a0fec4c14b28
karmicDev <m.karbe@tignum.com>
2017-04-05 09:36:38 +0200

    IT-300: Add mockdata

--- QOT/Me/MeSectionViewModel.swift
+++ QOT/Me/MeSectionViewModel.swift
@@ -27,5 +27,66 @@ final class MeSectionViewModel {
 
 enum MyDataItem {
     case image(localID: String, placeholderURL: URL)
-    case dataPoint(localID: String, distance: Float, angle: Float)
+    case dataPoint(localID: String, title: String, distance: Float, angle: Float, category: Category)
+
+    enum Category {
+        case load
+        case brainBody
+    }
+}
+
+private var mockMyDataItems: [MyDataItem] {
+    return [
+        .image(
+            localID: UUID().uuidString, placeholderURL: URL(string:"https://example.com")!
+        ),
+
+        .dataPoint(
+            localID: UUID().uuidString,
+            title: "Meeting",
+            distance: 0.9,
+            angle: 0.45,
+            category: .load
+        ),
+
+        .dataPoint(
+            localID: UUID().uuidString,
+            title: "Travel",
+            distance: 0.8,
+            angle: 0.6,
+            category: .load
+        ),
+
+        .dataPoint(
+            localID: UUID().uuidString,
+            title: "Intensity",
+            distance: 0.7,
+            angle: 0.5,
+            category: .load
+        ),
+
+        .dataPoint(
+            localID: UUID().uuidString,
+            title: "Peek Performance",
+            distance: 0.6,
+            angle: 0.8,
+            category: .load
+        ),
+
+        .dataPoint(
+            localID: UUID().uuidString,
+            title: "Sleep",
+            distance: 0.5,
+            angle: 0.3,
+            category: .brainBody
+        ),
+
+        .dataPoint(
+            localID: UUID().uuidString,
+            title: "Activity",
+            distance: 0.4,
+            angle: 0.9,
+            category: .brainBody
+        )
+    ]
 }
