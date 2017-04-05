feea68dc146835c5a5e6821e63bd7e890a682879
tignum <tignum@tignums-Mac-mini.local>
2017-04-05 16:00:29 +0200

    created a new group as topbarviewcontroller

---
+++ QOT/Learn/Learn Strategy/TopBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TopBarViewController.swift
+//  QOT
+//
+//  Created by tignum on 4/5/17.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+class TopBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
