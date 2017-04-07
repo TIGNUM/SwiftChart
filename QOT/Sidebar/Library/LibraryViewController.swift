3e4758330cde1121d818cafeaac70a6446aeb0d1
tignum <tignum@tignums-Mac-mini.local>
2017-04-07 12:20:29 +0200

    added a table view controller with xib

--- QOT/Sidebar/Library/LibraryViewController.swift
+++ QOT/Sidebar/Library/LibraryViewController.swift
@@ -14,33 +14,57 @@ protocol LibraryViewControllerDelegate: class {
 }
 
 final class LibraryViewController: UIViewController {
-
+    
     // MARK: - Properties
-
+    @IBOutlet weak var tableView: UITableView!
     let viewModel: LibraryViewModel
     weak var delegate: LibraryViewControllerDelegate?
-
+    
     // MARK: - Life Cycle
-
+    
     init(viewModel: LibraryViewModel) {
         self.viewModel = viewModel
-
+        
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeView))
         view.addGestureRecognizer(tapGestureRecognizer)
         view.backgroundColor = .black
     }
-
+    
     func closeView(gestureRecognizer: UITapGestureRecognizer) {
         delegate?.didTapClose(in: self)
+    }
+}
+
+extension LibraryViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    }
+    
+}
+
+extension LibraryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: LearnStrategyTextCell = tableView.dequeueCell(for: indexPath)
+        return cell
     }
 }
