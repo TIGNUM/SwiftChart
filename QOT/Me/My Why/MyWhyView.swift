1b8b27ca6c4530533a2b178b4f5263953718cac2
karmicDev <m.karbe@tignum.com>
2017-04-13 12:19:37 +0200

    IT-350: Add new file MyWhyView

---
+++ QOT/Me/My Why/MyWhyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyWhyView.swift
+//  QOT
+//
+//  Created by karmic on 13.04.17.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+class MyWhyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
