081d4f379fb2bda79b7008f6b55b6d729c6c57af
Kamil Sokołowski <kamil.sokolowski@type-it.pl>
2017-04-13 16:09:49 +0200

    implementation of Prepare for event View

---
+++ QOT/Prepare/PrepareTrips/PrepareEventAddNewTripTableViewCell/PrepareEventAddNewTripTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PrepareEventAddNewTripTableViewCell.swift
+//  QOT
+//
+//  Created by Type-IT on 13.04.2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+class PrepareEventAddNewTripTableViewCell: UITableViewCell, Dequeueable {
+
+    @IBOutlet weak var addNewTripLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- QOT/Prepare/PrepareTrips/PrepareEventsViewController.swift
+++ QOT/Prepare/PrepareTrips/PrepareEventsViewController.swift
@@ -13,10 +13,11 @@ protocol PrepareEventsViewControllerDelegate: class {
     func didTapItem(item: PrepareEventsItem, in viewController: PrepareEventsViewController)
 }
 
-final class PrepareEventsViewController: UIViewController {
+final class PrepareEventsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // MARK: - Properties
 
+    @IBOutlet weak var tableView: UITableView!
     let viewModel: PrepareEventsViewModel
     weak var delegate: PrepareEventsViewControllerDelegate?
 
@@ -35,12 +36,66 @@ final class PrepareEventsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeView))
-        view.addGestureRecognizer(tapGestureRecognizer)
         view.backgroundColor = .black
+
+        tableView.registerDequeueable(PrepareEventsUpcomingTripTableViewCell.self)
+        tableView.registerDequeueable(PrepareEventAddNewTripTableViewCell.self)
+        tableView.registerDequeueable(PrepareEventSimpleTableViewCell.self)
+
+        let nib = UINib(nibName: String(describing: PrepareEventHeaderTableViewCell.self), bundle: nil)
+        tableView.register(nib, forHeaderFooterViewReuseIdentifier: String(describing: PrepareEventHeaderTableViewCell.self))
     }
 
     func closeView(gestureRecognizer: UITapGestureRecognizer) {
         delegate?.didTapClose(in: self)
+    }
+}
+
+extension PrepareEventsViewController {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.sectionCount
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.itemCount(section: section)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = viewModel.item(at: indexPath)
+
+        switch item {
+        case .event(let event):
+            let cell: PrepareEventsUpcomingTripTableViewCell = tableView.dequeueCell(for: indexPath)
+            cell.titleLabel.text = event.title
+            cell.dateLabel.text = event.subtitle
+            return cell
+
+        case .addEvent:
+            let cell: PrepareEventAddNewTripTableViewCell = tableView.dequeueCell(for: indexPath)
+            return cell
+
+        case .addReminder(let title):
+            let cell: PrepareEventSimpleTableViewCell = tableView.dequeueCell(for: indexPath)
+            cell.titleLabel.text = title
+            return cell
+
+        case .saveToPDF(let title):
+            let cell: PrepareEventSimpleTableViewCell = tableView.dequeueCell(for: indexPath)
+            cell.titleLabel.text = title
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let item = viewModel.title(section: section)
+
+        guard let cell: PrepareEventHeaderTableViewCell = self.tableView.dequeueReusableHeaderFooterView(withIdentifier: String(describing: PrepareEventHeaderTableViewCell.self)) as? PrepareEventHeaderTableViewCell
+            else {
+                fatalError("cannotCast")
+        }
+        cell.headerTitleLabel.text = item
+
+        return cell
     }
 }
