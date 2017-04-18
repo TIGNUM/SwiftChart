29d5d3d9239ba0fbd497e8989540a6e15f03f7b1
karmicDev <m.karbe@tignum.com>
2017-04-18 16:40:12 +0200

    IT-335: Update WeeklyChoiceViewModel

--- QOT/Me/My Universe/Weekly Choices/WeeklyChoicesViewModel.swift
+++ QOT/Me/My Universe/Weekly Choices/WeeklyChoicesViewModel.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import ReactiveKit
+
+final class WeeklyChoicesViewModel {
+
+    // MARK: - Properties
+
+    let items = weeklyChoices
+    let updates = PublishSubject<CollectionUpdate, NoError>()
+
+    var itemCount: Int {
+        return items.count
+    }
+
+    func item(at index: Index) -> WeeklyChoice {
+        return items[index]
+    }
+}
+
+private var weeklyChoices: [WeeklyChoice] {
+    var choices = [WeeklyChoice]()
+
+    for index in 1...365 {
+        let titleIndex = index < 10 ? ("0" + "\(index)") : "\(index)"
+        let weeklyChoice =  MockWeeklyChoice(
+            localID: UUID().uuidString,
+            title: ".\(titleIndex) choice",
+            text: "You are having a Lorem ipsum here and"
+        )
+        choices.append(weeklyChoice)
+    }
+
+    return choices
+}
