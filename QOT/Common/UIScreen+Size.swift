32957703b92a3f32f13b625b868c11daddcff02e
karmicDev <m.karbe@tignum.com>
2017-04-19 10:14:53 +0200

    IT-368: Add extension UIscreeen

---
+++ QOT/Common/UIScreen+Size.swift
@@ -0,0 +1,27 @@
+//
+//  UIScreen+Size.swift
+//  QOT
+//
+//  Created by karmic on 19.04.17.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - UIScreen
+
+extension UIScreen {
+
+    class var isBigScreen: Bool {
+        return false
+    }
+
+    class var isMediumScreen: Bool {
+        return false
+    }
+
+    class var isSmallScreen: Bool {
+        return false
+    }
+}
