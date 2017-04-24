2e66429542a8329263e4502ec671e47706d0df35
karmicDev <m.karbe@tignum.com>
2017-04-24 13:23:31 +0200

    feature/IT-382: Add new file: PrepareContentCoordinator

---
+++ QOT/Prepare/PrepareContent/PrepareContentCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  PrepareContentCoordinator.swift
+//  QOT
+//
+//  Created by karmic on 24.04.17.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class PrepareContentCoordinator: ParentCoordinator {
+
+    // MARK: - Properties
+
+    fileprivate let rootViewController: UIViewController
+    fileprivate let services: Services
+    fileprivate let eventTracker: EventTracker
+
+    var children: [Coordinator] = []
+
+    // MARK: - Life Cycle
+
+    init(root: UIViewController, services: Services, eventTracker: EventTracker) {
+        self.rootViewController = root
+        self.services = services
+        self.eventTracker = eventTracker
+    }
+
+    func start() {
+        
+    }
+}
