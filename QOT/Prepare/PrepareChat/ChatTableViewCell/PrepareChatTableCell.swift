5792e9993a5b6a9d101a5f4ea27e1ff5fc686dfe
kingaamir29 <a.suhial@tignum.com>
2017-05-02 10:01:00 +0200

    Feature/it 384/chat cell (#77)
    * implement custom DashPatternView Class
    fixed the style of border
    
    * fixed the suggestions…
    
    * updated … however now we are not using the DashedPatternVIEW i used just a UI View…
    
    * feature/IT-408: removed wired file
    
    * Merge branch 'dev' into feature/IT-384
    
    # Conflicts:
    #	QOT.xcodeproj/project.pbxproj
    #	QOT/Prepare/PrepareChat/ChatTableViewCell/ChatTableViewCell.xib
    
    * Remove changes to existings xibs

---
+++ QOT/Prepare/PrepareChat/ChatTableViewCell/PrepareChatTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  PrepareChatTableCell.swift
+//  QOT
+//
+//  Created by tignum on 4/21/17.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+final class PrepareChatTableCell: UITableViewCell, Dequeueable {
+
+    @IBOutlet fileprivate weak var dashedView: UIView!
+    @IBOutlet fileprivate weak var dashedChatLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+    func updateChatLabel(with text: String) {
+       dashedChatLabel.text = text
+    }
+}
