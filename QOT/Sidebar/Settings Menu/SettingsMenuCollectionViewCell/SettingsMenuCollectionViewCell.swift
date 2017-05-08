515dfc0a6e2dbb64ad69759b3c9a6fc9a0aa6af4
Kamil Sokołowski <kamil.sokolowski@type-it.pl>
2017-05-08 10:49:50 +0200

    Feature/it 363 (#66)
    * main View implementation
    
    * view implementations + text editing
    
    * layout changes
    
    * cell layout update
    
    * adding missing files
    
    * bundle ID
    
    * Run synx
    
    * requested change number One
    
    * requested localized text changes
    
    * attributed text
    
    * Remove title and close button from settings menu
    
    This is provided by nav bar
    
    * Fix settings menu collection view right inset

---
+++ QOT/Sidebar/Settings Menu/SettingsMenuCollectionViewCell/SettingsMenuCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsMenuCollectionViewCell.swift
+//  QOT
+//
+//  Created by Type-IT on 20.04.2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+class SettingsMenuCollectionViewCell: UICollectionViewCell, Dequeueable {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var cardView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        cardView.layer.cornerRadius = 10
+    }
+}
--- QOT/Sidebar/Settings Menu/SettingsMenuViewController.swift
+++ QOT/Sidebar/Settings Menu/SettingsMenuViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import ReactiveKit
 import Bond
+import Kingfisher
 
 protocol SettingsMenuViewControllerDelegate: class {
     func didTapGeneral(in viewController: SettingsMenuViewController)
@@ -16,12 +17,22 @@ protocol SettingsMenuViewControllerDelegate: class {
     func didTapSecurity(in viewController: SettingsMenuViewController)
 }
 
-final class SettingsMenuViewController: UIViewController {
+final class SettingsMenuViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     // MARK: - Properties
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var imgeView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+
+    @IBOutlet weak var generalButton: UIButton!
+    @IBOutlet weak var notificationsButton: UIButton!
+    @IBOutlet weak var securityButton: UIButton!
+
     private let disposeBag = DisposeBag()
-    private let viewModel: SettingsMenuViewModel
+    fileprivate let viewModel: SettingsMenuViewModel
+
     weak var delegate: SettingsMenuViewControllerDelegate?
 
     init(viewModel: SettingsMenuViewModel) {
@@ -36,44 +47,66 @@ final class SettingsMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.registerDequeueable(SettingsMenuCollectionViewCell.self)
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 29, bottom: 0, right: 29)
 
-        view.backgroundColor = .purple
-        settingsButtons().forEach { (button: UIButton) in
-            view.addSubview(button)
-            print(button)
-        }
+        imgeView.kf.setImage(with: viewModel.profile.photoURL)
+        imgeView.layer.cornerRadius = 10
+
+        titleLabel.attributedText = prepareAndSetTextAttributes(string: viewModel.profile.name.uppercased(), letterSpacing: -2, font: UIFont(name:"Simple-Regular", size:32.0), lineSpacing: 0, textColor: nil)
+        positionLabel.attributedText = prepareAndSetTextAttributes(string: viewModel.profile.position.uppercased(), letterSpacing: 2, font: UIFont(name:"BentonSans", size:11.0), lineSpacing: 4, textColor: nil)
+
+        generalButton.setAttributedTitle(prepareAndSetTextAttributes(string: R.string.localized.sidebarSettingsMenuGeneralButton().uppercased(), letterSpacing: -0.8, font:  UIFont(name:"Simple-Regular", size:20.0), lineSpacing: 0, textColor: nil), for: UIControlState.normal)
+        notificationsButton.setAttributedTitle(prepareAndSetTextAttributes(string: R.string.localized.sidebarSettingsMenuNotificationsButton().uppercased(), letterSpacing: -0.8, font:  UIFont(name:"Simple-Regular", size:20.0), lineSpacing: 0, textColor: nil), for: UIControlState.normal)
+        securityButton.setAttributedTitle(prepareAndSetTextAttributes(string: R.string.localized.sidebarSettingsMenuSecurityButton().uppercased(), letterSpacing: -0.8, font:  UIFont(name:"Simple-Regular", size:20.0), lineSpacing: 0, textColor: nil), for: UIControlState.normal)
+
     }
 
-    // TODO: Please remove, just for testing.
+    func prepareAndSetTextAttributes(string: String, letterSpacing: CGFloat, font: UIFont?, lineSpacing: CGFloat, textColor: UIColor?) -> NSMutableAttributedString {
+        let defaultFont: UIFont = UIFont(name:"Simple-Regular", size: 14.0)!
+        let defaultColor: UIColor = UIColor.white
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        let attrString = NSMutableAttributedString(string: string)
+        attrString.addAttribute(NSKernAttributeName, value: letterSpacing, range: NSRange(location: 0, length: string.utf16.count))
+        attrString.addAttributes([NSFontAttributeName: font ?? defaultFont], range: NSRange(location: 0, length: string.utf16.count))
+        attrString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: string.utf16.count))
+        attrString.addAttributes([NSForegroundColorAttributeName: textColor ?? defaultColor], range: NSRange(location: 0, length: string.utf16.count))
 
-    private func settingsButtons() -> [UIButton] {
-        let genralButton = UIButton()
-        genralButton.setTitle("GENERAL", for: .normal)
-        genralButton.addTarget(self, action: #selector(didTapSettingButton), for: .touchUpInside)
-        genralButton.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
-        genralButton.sizeToFit()
-
-        let notificationsButton = UIButton()
-        notificationsButton.setTitle("NOTIFICATIONS", for: .normal)
-        notificationsButton.addTarget(self, action: #selector(didTapSettingButton), for: .touchUpInside)
-        notificationsButton.frame = CGRect(x: 100, y: 200, width: 0, height: 0)
-        notificationsButton.sizeToFit()
-
-        let securityButton = UIButton()
-        securityButton.setTitle("SECURITY", for: .normal)
-        securityButton.addTarget(self, action: #selector(didTapSettingButton), for: .touchUpInside)
-        securityButton.frame = CGRect(x: 100, y: 300, width: 0, height: 0)
-        securityButton.sizeToFit()
-
-        return [genralButton, notificationsButton, securityButton]
+        return attrString
     }
 
-    func didTapSettingButton(sender: UIButton) {
-        switch sender.titleLabel?.text ?? String() {
-            case "GENERAL": delegate?.didTapGeneral(in: self)
-            case "NOTIFICATIONS": delegate?.didTapSecurity(in: self)
-            case "SECURITY": delegate?.didTapNotifications(in: self)
-            default: return
-        }
+    // MARK: - delegateMethods
+
+    @IBAction func generalAction(_ sender: Any) {
+        delegate?.didTapGeneral(in: self)
+    }
+
+    @IBAction func notyficationAction(_ sender: Any) {
+        delegate?.didTapNotifications(in: self)
+    }
+
+    @IBAction func securityAction(_ sender: Any) {
+        delegate?.didTapSecurity(in: self)
+    }
+}
+
+extension SettingsMenuViewController {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.tileCount
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = viewModel.tile(at: indexPath.row)
+        let cell: SettingsMenuCollectionViewCell = collectionView.dequeueCell(for: indexPath)
+        
+        cell.timeLabel.attributedText = prepareAndSetTextAttributes(string: item.title, letterSpacing: 1, font: UIFont(name:"Simple-Regular", size:14.0), lineSpacing: 2, textColor: nil)
+        cell.titleLabel.attributedText = prepareAndSetTextAttributes(string: item.subtitle.uppercased(), letterSpacing: 2, font: UIFont(name:"BentonSans", size: 11.0), lineSpacing: 3.2, textColor: UIColor.init(colorLiteralRed: 255.0, green: 255.0, blue: 255.0, alpha: 0.3))
+
+        return cell
     }
 }
