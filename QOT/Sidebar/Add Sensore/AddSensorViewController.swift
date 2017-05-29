61bad16867cf1ac59204bfa39adc2d02a15b93ce
Sam Wyndham <samwyndham@users.noreply.github.com>
2017-05-29 15:16:20 +0200

    Feature/it 481 - Add sensor view controller skeleton (#128)
    * Sensor(e)

--- QOT/Sidebar/Add Sensore/AddSensorViewController.swift
+++ QOT/Sidebar/Add Sensore/AddSensorViewController.swift
@@ -14,7 +14,12 @@ protocol AddSensorViewControllerDelegate: class {
 
 final class AddSensorViewController: UIViewController {
 
-    private let viewModel: AddSensorViewModel
+    fileprivate let viewModel: AddSensorViewModel
+
+    @IBOutlet var viewTitleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
 
     weak var delegate: AddSensorViewControllerDelegate?
 
@@ -30,13 +35,43 @@ final class AddSensorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewTitleLabel.attributedText = prepareAndSetTextAttributes(string: "SENSORS", letterSpacing: 2, font: UIFont(name:"Simple-Regular", size: 24.0), lineSpacing: 0)
+        headerLabel.attributedText = prepareAndSetTextAttributes(string: viewModel.heading.uppercased(), letterSpacing: 2, font: UIFont(name:"Simple-Regular", size: 24.0), lineSpacing: 0)
+        textLabel.attributedText = prepareAndSetTextAttributes(string: viewModel.text, letterSpacing: 0, font: UIFont(name:"BentonSans-Book", size: 16.0), lineSpacing: 0)
 
-        // Do any additional setup after loading the view.
+        collectionView.registerDequeueable(SensorCollectionViewCell.self)
+        collectionView.contentInset.left = 36.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func prepareAndSetTextAttributes(string: String, letterSpacing: CGFloat, font: UIFont?, lineSpacing: CGFloat) -> NSMutableAttributedString {
+        let defaultFont: UIFont = UIFont(name:"Simple-Regular", size: 14.0)!
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+
+        let attrString = NSMutableAttributedString(string: string)
+        attrString.addAttribute(NSKernAttributeName, value: letterSpacing, range: NSRange(location: 0, length: string.utf16.count))
+        attrString.addAttributes([NSFontAttributeName: font ?? defaultFont], range: NSRange(location: 0, length: string.utf16.count))
+        attrString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: string.utf16.count))
+
+        return attrString
+    }
+}
+extension AddSensorViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.itemCount
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = viewModel.item(at: indexPath.row)
+        let cell: SensorCollectionViewCell = collectionView.dequeueCell(for: indexPath)
+
+        cell.imageView.image = item.image
+
+        return cell
+    }
 }
---
+++ QOT/Sidebar/Add Sensore/Cell/SensorCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SensorCollectionViewCell.swift
+//  QOT
+//
+//  Created by Type-IT on 22.05.2017.
+//  Copyright © 2017 Tignum. All rights reserved.
+//
+
+import UIKit
+
+class SensorCollectionViewCell: UICollectionViewCell, Dequeueable {
+
+    @IBOutlet var imageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
