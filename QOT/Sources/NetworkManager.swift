46840a7d2fdbdf8c3e33a92e0cacc0723366500a
Moucheg Mouradian <mouchegmouradian@users.noreply.github.com>
2017-06-07 12:47:36 +0200

    Feature/it 560 - Cancelable requests (#163)
    * UnitTests for NetworkManager
    
    * Network requests are now cancelable.
    UT added to test request cancelling
    
    * SerialRequest, which is returned by NetworkManager's request methods, conforms to Cancelable protocol
    
    * corrected an error in NetworkManager UT
    
    * Simplify requests
    
    * Simplify request

--- QOT/Sources/NetworkManager.swift
+++ QOT/Sources/NetworkManager.swift
@@ -56,9 +56,11 @@ final class NetworkManager {
         return serialRequest
     }
 
-    func request<T: JSONDecodable>(token: String, endpoint: Endpoint, page: Int, accumulator: [DownSyncChange<T>] = [], completion: @escaping (Result<[DownSyncChange<T>], NetworkError>) -> Void) {
+    @discardableResult func request<T: JSONDecodable>(token: String, endpoint: Endpoint, page: Int, accumulator: [DownSyncChange<T>] = [], serialRequest: SerialRequest? = nil, completion: @escaping (Result<[DownSyncChange<T>], NetworkError>) -> Void) -> SerialRequest {
+        let serialRequest = serialRequest ?? SerialRequest()
+
         let urlRequest = DownSyncRequest(endpoint: endpoint, syncToken: token, page: 1)
-        self.request(urlRequest, parser: DownSyncResultParser<T>.parse) { [weak self] (result) in
+        serialRequest.request = self.request(urlRequest, parser: DownSyncResultParser<T>.parse) { [weak self] (result) in
             switch result {
             case .success(let value):
                 let changes = accumulator + value.items
@@ -66,12 +68,13 @@ final class NetworkManager {
                     completion(.success(changes))
                 } else {
                     let page = page + 1
-                    self?.request(token: token, endpoint: endpoint, page: page, accumulator: changes, completion: completion)
+                    self?.request(token: token, endpoint: endpoint, page: page, accumulator: changes, serialRequest: serialRequest, completion: completion)
                 }
             case .failure(let error):
                 completion(.failure(error))
             }
-        }
+        }.request
+        return serialRequest
     }
 
     // MARK: Private
