2f0672cb74327e9c4468b8c2e426de14488f0675
Sam Wyndham <s.wyndham@tignum.com>
2017-09-13 18:19:52 +0200

    Delete unused function

--- QOT/Sources/AuthenticationTokenParser.swift
+++ QOT/Sources/AuthenticationTokenParser.swift
@@ -14,13 +14,6 @@ struct AuthenticationTokenParser {
         case failedToCreateTokenFromData(data: Data)
     }
 
-    static func parse(_ data: Data) throws -> String {
-        guard let token = String(data: data, encoding: .utf8) else {
-            throw Error.failedToCreateTokenFromData(data: data)
-        }
-        return token
-    }
-
     static func make(username: String, password: String) -> (Data) throws -> Credential {
         return { (data) in
             guard let token = String(data: data, encoding: .utf8) else {
