1d02a9e8f5a43b2d8091ff029aab4f6859dd8bc0
Michael Karbe <karmicDev@users.noreply.github.com>
2018-01-23 06:01:31 -0500

    Feature/qot 440 daily prep value key order (#830)
    * Update seeded data base.
    
    * Update seeded data base.

--- QOT/Sources/DownSyncResultParser.swift
+++ QOT/Sources/DownSyncResultParser.swift
@@ -23,7 +23,7 @@ struct DownSyncResult<T: DownSyncIntermediary> {
             let remoteID: Int = try json.getItemValue(at: T.remoteIDKey)
             switch syncStatus {
             case .created, .updated:
-                let createdAt = Date()//Date = try json.getDate(at: .createdAt)
+                let createdAt = try json.getDate(at: .createdAt, alongPath: .nullBecomesNil) ?? Date()
                 let modifiedAt = try json.getDate(at: .modifiedAt, alongPath: .nullBecomesNil) ?? createdAt
                 let data: T = try T(json: json)
 
--- QOT/Sources/LaunchHandler.swift
+++ QOT/Sources/LaunchHandler.swift
@@ -20,7 +20,7 @@ final class LaunchHandler {
     }
 
     func process(url: URL, notificationID: String = "", guideItem: Guide.Item? = nil) {
-        log("dailyPrep://LaunchHandler, process url:: \(url), notificationID: \(notificationID), guideItem: \(guideItem)")
+        log("dailyPrep://LaunchHandler, process url:: \(url), notificationID: \(notificationID), guideItem: \(String(describing: guideItem))")
         guard
             let host = url.host,
             let scheme = URLScheme(rawValue: host) else { return }
@@ -153,7 +153,7 @@ extension LaunchHandler {
 extension LaunchHandler {
 
     func dailyPrep(groupID: String?, notificationID: String, guideItem: Guide.Item?) {
-        log("dailyPrep://LaunchHandler, dailyPrep groupID:: \(groupID), notificationID: \(notificationID), guideItem: \(guideItem)")
+        log("dailyPrep://LaunchHandler, dailyPrep groupID:: \(String(describing: groupID)), notificationID: \(notificationID), guideItem: \(String(describing: guideItem))")
         guard
             let groupID = groupID,
             let groupIDIntValue = Int(groupID) else { return }
