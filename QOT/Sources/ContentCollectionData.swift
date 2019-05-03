1efcc6b8bc09cec7e38c3d8c50f4e6a7ad4f8f95
Michael Karbe <karmicDev@users.noreply.github.com>
2019-05-03 11:40:16 +0200

    Removed relatedContentIds (#1556)

--- QOT/Sources/ContentCollectionData.swift
+++ QOT/Sources/ContentCollectionData.swift
@@ -17,7 +17,6 @@ struct ContentCollectionData {
     let title: String
     let layoutInfo: String?
     let searchTags: String
-    let relatedContentIDs: String
     let categoryIDs: [Int]
     let thumbnailURLString: String?
     let relatedContentList: [ContentRelationIntermediary]
@@ -34,7 +33,6 @@ extension ContentCollectionData: DownSyncIntermediary {
         self.section = try json.getItemValue(at: .section)
         self.title = try json.getItemValue(at: .title)
         self.sortOrder = try json.getItemValue(at: .sortOrder)
-        self.relatedContentIDs = try json.serializeString(at: .relatedContentIds)
         self.searchTags = try (json.getArray(at: .searchTags) as [String]).joined(separator: ",")
         self.categoryIDs = try json.getArray(at: .categoryIds)
         self.layoutInfo = try json.serializeString(at: .layoutInfo)
