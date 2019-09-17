b238963d24d3e7f856f9953ef03f466da3704252
Michael Karbe <michaelkarbe@Michaels-MBP.fritz.box>
2019-09-17 14:22:47 +0200

    Add DTSprintRefelction and hooked it up.

---
+++ QOT/Sources/DTSprintReflectionInteractor.swift
@@ -0,0 +1,41 @@
+//
+//  DTSprintReflectionInteractor.swift
+//  QOT
+//
+//  Created by Michael Karbe on 17.09.19.
+//  Copyright (c) 2019 Tignum. All rights reserved.
+//
+
+import UIKit
+import qot_dal
+
+final class DTSprintReflectionInteractor: DTInteractor {
+    
+    // MARK: - Properties
+    var sprint: QDMSprint?
+    weak var sprintRefelctionPresenter: DTSprintReflectionPresenter?
+
+    override init(_ presenter: DTPresenterInterface, questionGroup: QuestionGroup, introKey: String) {
+        super.init(presenter, questionGroup: questionGroup, introKey: introKey)
+    }
+
+    override func loadIntroQuestion(_ firstQuestion: QDMQuestion?) {
+        let questionUpdate = getTitleUpdate(selectedAnswers: [], questionKey: nil)
+        let presentationModel = createPresentationModel(questionId: firstQuestion?.remoteID ?? 0,
+                                                        answerFilter: introKey,
+                                                        questionUpdate: questionUpdate,
+                                                        questions: questions)
+        let node = Node(questionId: firstQuestion?.remoteID,
+                        answerFilter: nil,
+                        titleUpdate: questionUpdate)
+        presentedNodes.append(node)
+        sprintRefelctionPresenter?.showNextQuestion(presentationModel, isDark: isDark)
+    }
+    
+    override func getTitleUpdate(selectedAnswers: [DTViewModel.Answer], questionKey: String?) -> String? {
+        return sprint?.title
+    }
+}
+
+// MARK: - DTSprintReflectionInteractorInterface
+extension DTSprintReflectionInteractor: DTSprintReflectionInteractorInterface {}
--- QOT/Sources/MySprints/MySprintDetails/MySprintDetailsViewController.swift
+++ QOT/Sources/MySprints/MySprintDetails/MySprintDetailsViewController.swift
@@ -128,10 +128,12 @@ extension MySprintDetailsViewController: MySprintDetailsViewControllerInterface 
 
     func trackSprintPause() {
         trackUserEvent(QDMUserEventTracking.Name.PAUSE, action: QDMUserEventTracking.Action.TAP)
+        interactor?.didTapItemAction(1)
     }
 
     func trackSprintContinue() {
         trackUserEvent(QDMUserEventTracking.Name.CONTINUE, action: QDMUserEventTracking.Action.TAP)
+        interactor?.didTapItemAction(2)
     }
 
     func trackSprintStart() {
