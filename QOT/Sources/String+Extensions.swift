34b2acc3e883c4f1e4c9a67eae361f035ee46ad8
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2019-09-18 17:26:10 +0200

    capitalized first letter of hintlabel in daily checkin

--- QOT/Sources/String+Extensions.swift
+++ QOT/Sources/String+Extensions.swift
@@ -202,3 +202,14 @@ extension String {
         return attributedString
     }
 }
+
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
