38c8166f22436c41f2a639259c5b93092e9b83b4
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2019-09-24 17:11:15 +0200

    adapted height

---
+++ QOT/Sources/ArrowButton.swift
@@ -0,0 +1,19 @@
+//
+//  ArrowButton.swift
+//  QOT
+//
+//  Created by Anais Plancoulaine on 24.09.19.
+//  Copyright © 2019 Tignum. All rights reserved.
+//
+
+import Foundation
+
+class ArrowButton : AnimatedButton {
+
+
+    override func layoutSubviews() {
+
+        
+        super.layoutSubviews()
+    }
+}
