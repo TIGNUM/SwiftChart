5de4c68bb7dc349f2d0c684112f39a45d9289958
Sanggeon Park <gunnih@gmail.com>
2019-10-08 13:44:26 +0200

    Revert "Ooops"
    This reverts commit 232ffc8f422e26544e237521fe240381f3d0c5ad.

--- QOT/Sources/SearchViewController.swift
+++ QOT/Sources/SearchViewController.swift
@@ -66,6 +66,7 @@ final class SearchViewController: BaseViewController, ScreenZLevelOverlay, Searc
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = true
+//        doActivate()
         deactivate(animated: false)
     }
 
