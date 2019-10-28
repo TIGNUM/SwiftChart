d2ea307b0a2143804e996f15050628abb7b3495c
Simu Voicu-Mircea <voicu.simu@hotmail.com>
2019-10-28 10:38:32 +0100

    Added reusable Base header view to Calendar Permission

--- QOT/Sources/PermissionCalendarView.swift
+++ QOT/Sources/PermissionCalendarView.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import qot_dal
 
 final class PermissionCalendarView: UIView {
 
     // MARK: - Properties
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var subtitleLabel: UILabel!
+    @IBOutlet weak var headerContainerView: UIView!
+    var baseHeaderView: QOTBaseHeaderView?
     @IBOutlet private weak var weekDayLabel: UILabel!
     @IBOutlet private weak var aboutLabel: UILabel!
     @IBOutlet private weak var listLabel01: UILabel!
@@ -24,11 +25,23 @@ final class PermissionCalendarView: UIView {
     static func instantiateFromNib() -> PermissionCalendarView {
         guard let view = R.nib.permissionCalendarView.instantiate(withOwner: self)
             .first as? PermissionCalendarView else { fatalError("Cannot load PermissionCalendarView") }
+        view.baseHeaderView = R.nib.qotBaseHeaderView.firstView(owner: self)
+        view.baseHeaderView?.addTo(superview: view.headerContainerView)
+        view.configure()
+
         return view
     }
 
     // MARK: Configuration
     func configure() {
-        ///TODO Create CMS content https://tignum.atlassian.net/browse/QOT-1702
+        baseHeaderView?.configure(title: AppTextService.get(AppTextKey.calendar_permission_view_title_header),
+                                  subtitle: AppTextService.get(AppTextKey.calendar_permission_view_subtitle_header),
+                                  darkMode: true)
+        weekDayLabel.text = AppTextService.get(AppTextKey.calendar_permission_view_body_weekday)
+        aboutLabel.text = AppTextService.get(AppTextKey.calendar_permission_view_body_title)
+        listLabel01.text = AppTextService.get(AppTextKey.calendar_permission_view_body_list_first)
+        listLabel02.text = AppTextService.get(AppTextKey.calendar_permission_view_body_list_second)
+        listLabel03.text = AppTextService.get(AppTextKey.calendar_permission_view_body_list_third)
+        listLabel04.text = AppTextService.get(AppTextKey.calendar_permission_view_body_list_fourth)
     }
 }
