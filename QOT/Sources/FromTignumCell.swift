6c96f90be988cd05098b30290241cb40401c9dd6
Simu Voicu-Mircea <voicu.simu@hotmail.com>
2019-10-30 16:05:42 +0100

    From Tignum

--- QOT/Sources/FromTignumCell.swift
+++ QOT/Sources/FromTignumCell.swift
@@ -10,39 +10,36 @@ import UIKit
 
 final class FromTignumCell: BaseDailyBriefCell {
 
-    @IBOutlet private weak var titleLabel: UILabel!
-//    add the height constraint to the label in the cell.
-    @IBOutlet private weak var heightOfText: NSLayoutConstraint!
+    @IBOutlet var headerHeightConstraint: NSLayoutConstraint!
+    var baseHeaderview: QOTBaseHeaderView?
+    @IBOutlet weak var headerView: UIView!
     @IBOutlet private weak var fromTignumText: UILabel!
-    private var isLabelAtMaxHeight = false
+    private var detailsExpanded = false
     @IBOutlet private weak var button: AnimatedButton!
-    @IBOutlet weak var fromTignumTitle: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         ThemeBorder.accent.apply(button)
-        skeletonManager.addTitle(titleLabel)
+        baseHeaderview = R.nib.qotBaseHeaderView.firstView(owner: self)
+        baseHeaderview?.addTo(superview: headerView, showSkeleton: true)
         skeletonManager.addSubtitle(fromTignumText)
-        skeletonManager.addSubtitle(fromTignumTitle)
         skeletonManager.addOtherView(button)
     }
 
     @IBAction func discoverButton(_ sender: Any) {
-        if isLabelAtMaxHeight {
-            isLabelAtMaxHeight = false
-//            heightOfText.constant = 100
+        if detailsExpanded {
+            detailsExpanded = false
         } else {
 //          link to more details
-            isLabelAtMaxHeight = true
-//          Right now we are not displaying the more button in from tignum
-//          heightOfText.constant = 400
+            detailsExpanded = true
         }
     }
 
     func configure(with viewModel: FromTignumCellViewModel?) {
         guard let model = viewModel else { return }
         skeletonManager.hide()
-        ThemeText.dailyBriefTitle.apply((model.title ?? "").uppercased(), to: titleLabel)
+        baseHeaderview?.configure(title: (model.title ?? "").uppercased(), subtitle: model.subtitle)
+        ThemeText.dailyBriefTitle.apply((model.title ?? "").uppercased(), to: baseHeaderview?.titleLabel)
         ThemeText.bespokeText.apply(model.text, to: fromTignumText)
     }
 }
