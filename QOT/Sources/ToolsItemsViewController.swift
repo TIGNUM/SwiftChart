f6affc253c39284d470f512aa66a6adddf793a0d
karmic <karbemichael@gmail.com>
2019-11-19 10:58:07 +0100

    Some small clean swift refactore

--- QOT/Sources/ToolsItemsViewController.swift
+++ QOT/Sources/ToolsItemsViewController.swift
@@ -15,25 +15,24 @@ private enum ToolType: String {
     case pdf
 }
 
+
 protocol IsPlayingDelegate: class {
     func isPlaying(remoteID: Int?) -> Bool
 }
 
 final class ToolsItemsViewController: BaseWithTableViewController, ScreenZLevel3 {
 
-    // MARK: - Properties
-
-    var interactor: ToolsItemsInteractorInterface?
-    @IBOutlet weak var backButton: UIButton!
-    @IBAction func backButton(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
-    }
-    private var lastAudioIndexPath: IndexPath?
     private enum CellType: Int, CaseIterable {
         case header = 0
         case sections
     }
 
+    // MARK: - Properties
+    var interactor: ToolsItemsInteractorInterface!
+    @IBOutlet weak var backButton: UIButton!
+    private var lastAudioIndexPath: IndexPath?
+
+    // MARK: - Init
     init(configure: Configurator<ToolsItemsViewController>) {
         super.init(nibName: nil, bundle: nil)
         configure(self)
@@ -48,17 +47,19 @@ final class ToolsItemsViewController: BaseWithTableViewController, ScreenZLevel3
         ThemeView.qotTools.apply(view)
         ThemeView.qotTools.apply(tableView)
         setCustomBackButton()
-        interactor?.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(didEndAudio(_:)), name: .didEndAudio, object: nil)
+        interactor.viewDidLoad()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(didEndAudio(_:)),
+                                               name: .didEndAudio, object: nil)
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        if self == self.navigationController?.viewControllers.first {
+        if self == navigationController?.viewControllers.first {
             backButton.isHidden = true
         }
         super.viewWillAppear(animated)
         setCustomBackButton()
-        setStatusBar(colorMode: ColorMode.darkNot)
+        setStatusBar(colorMode: .darkNot)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -75,15 +76,13 @@ final class ToolsItemsViewController: BaseWithTableViewController, ScreenZLevel3
         pageTrack.pageId = 0
         pageTrack.pageKey = pageKey
         pageTrack.associatedValueType = .CONTENT_COLLECTION
-        pageTrack.associatedValueId = interactor?.selectedContentId()
+        pageTrack.associatedValueId = interactor.selectedContentId()
         NotificationCenter.default.post(name: .reportPageTracking, object: pageTrack)
     }
 }
 
 // MARK: - Private
-
 private extension ToolsItemsViewController {
-
     func setupTableView() {
         tableView.registerDequeueable(ToolsCollectionsAudioTableViewCell.self)
         tableView.registerDequeueable(ToolsCollectionsVideoTableViewCell.self)
@@ -92,8 +91,10 @@ private extension ToolsItemsViewController {
 }
 
 // MARK: - Actions
-
 private extension ToolsItemsViewController {
+    @IBAction func backButton(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
 
     @IBAction func closeButton(_ sender: Any) {
         trackUserEvent(.CLOSE, action: .TAP)
@@ -102,9 +103,7 @@ private extension ToolsItemsViewController {
 }
 
 // MARK: - CoachViewControllerInterface
-
 extension ToolsItemsViewController: ToolsItemsViewControllerInterface {
-
     func setupView() {
         setupTableView()
         setCustomBackButton()
@@ -125,17 +124,16 @@ extension ToolsItemsViewController: ToolsItemsViewControllerInterface {
 }
 
 extension ToolsItemsViewController: UITableViewDelegate, UITableViewDataSource {
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return interactor?.rowCount ?? 0
+        return interactor.rowCount
     }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let cellType = CellType.allCases[section]
         switch cellType {
         case .header:
-            return ToolsTableHeaderView.instantiateFromNib(title: interactor?.headerTitle ?? "",
-                                                           subtitle: interactor?.headerSubtitle ?? "")
+            return ToolsTableHeaderView.instantiateFromNib(title: interactor.headerTitle,
+                                                           subtitle: interactor.headerSubtitle)
         default: return nil
         }
     }
@@ -145,14 +143,14 @@ extension ToolsItemsViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tool = interactor?.tools[indexPath.item]
-        switch tool?.type {
+        let tool = interactor.tools[indexPath.item]
+        switch tool.type {
         case ToolType.video.rawValue:
             let cell: ToolsCollectionsVideoTableViewCell = tableView.dequeueCell(for: indexPath)
             cell.setSelectedColor(.accent, alphaComponent: 0.1)
-            cell.configure(title: tool?.title ?? "video title",
-                           timeToWatch: tool?.durationString ?? "",
-                           imageURL: tool?.imageURL)
+            cell.configure(title: tool.title,
+                           timeToWatch: tool.durationString,
+                           imageURL: tool.imageURL)
             cell.addTopLine(for: indexPath.row)
             cell.sizeToFit()
             cell.layoutIfNeeded()
@@ -160,24 +158,24 @@ extension ToolsItemsViewController: UITableViewDelegate, UITableViewDataSource {
         case ToolType.audio.rawValue:
             let cell: ToolsCollectionsAudioTableViewCell = tableView.dequeueCell(for: indexPath)
             cell.setSelectedColor(.accent, alphaComponent: 0.1)
-            cell.configure(categoryTitle: tool?.categoryTitle ?? "",
-                           title: tool?.title ?? "",
-                           timeToWatch: tool?.durationString ?? "",
-                           mediaURL: tool?.mediaURL,
-                           duration: tool?.duration ?? 0,
-                           remoteID: tool?.remoteID ?? 0,
+            cell.configure(categoryTitle: tool.categoryTitle,
+                           title: tool.title,
+                           timeToWatch: tool.durationString,
+                           mediaURL: tool.mediaURL,
+                           duration: tool.duration,
+                           remoteID: tool.remoteID,
                            delegate: self)
             cell.addTopLine(for: indexPath.row)
             return cell
         default:
             let cell: ToolsCollectionsAudioTableViewCell = tableView.dequeueCell(for: indexPath)
             cell.setSelectedColor(.accent, alphaComponent: 0.1)
-            cell.configure(categoryTitle: tool?.categoryTitle ?? "",
-                           title: tool?.title ?? "",
-                           timeToWatch: tool?.durationString ?? "",
-                           mediaURL: tool?.mediaURL,
-                           duration: tool?.duration ?? 0,
-                           remoteID: tool?.remoteID ?? 0,
+            cell.configure(categoryTitle: tool.categoryTitle,
+                           title: tool.title,
+                           timeToWatch: tool.durationString,
+                           mediaURL: tool.mediaURL,
+                           duration: tool.duration,
+                           remoteID: tool.remoteID,
                            delegate: nil)
             cell.addTopLine(for: indexPath.row)
             cell.makePDFCell()
@@ -187,14 +185,14 @@ extension ToolsItemsViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         didSelectRow(at: indexPath)
-        let tool = interactor?.tools[indexPath.item]
-        trackUserEvent(.OPEN, value: tool?.remoteID ?? 0, valueType: .CONTENT_ITEM, action: .TAP)
-        if let contentItemId = tool?.remoteID,
-            let launchURL = URLScheme.contentItem.launchURLWithParameterValue(String(contentItemId)) {
+        let tool = interactor.tools[indexPath.item]
+        trackUserEvent(.OPEN, value: tool.remoteID, valueType: .CONTENT_ITEM, action: .TAP)
+        if let launchURL = URLScheme.contentItem.launchURLWithParameterValue(String(tool.remoteID)) {
             UIApplication.shared.open(launchURL, options: [:], completionHandler: nil)
         }
     }
 }
+
 // MARK: - Audio Player Related
 extension ToolsItemsViewController {
     @objc func didEndAudio(_ notification: Notification) {
@@ -207,8 +205,7 @@ extension ToolsItemsViewController {
 }
 
 extension ToolsItemsViewController: IsPlayingDelegate {
-
     func isPlaying(remoteID: Int?) -> Bool {
-        return interactor?.isPlaying(remoteID: remoteID) ?? false
+        return interactor.isPlaying(remoteID: remoteID)
     }
 }
