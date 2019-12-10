a30b692f0123f92a8fbf450dc9bbf020fecb6573
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2019-12-10 09:44:25 +0100

    constraints

--- QOT/Sources/FromTignumCell.swift
+++ QOT/Sources/FromTignumCell.swift
@@ -15,15 +15,12 @@ final class FromTignumCell: BaseDailyBriefCell {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet private weak var fromTignumText: UILabel!
     private var detailsExpanded = false
-    @IBOutlet private weak var button: AnimatedButton!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        ThemeBorder.accent.apply(button)
         baseHeaderView = R.nib.qotBaseHeaderView.firstView(owner: self)
         baseHeaderView?.addTo(superview: headerView, showSkeleton: true)
         skeletonManager.addSubtitle(fromTignumText)
-        skeletonManager.addOtherView(button)
     }
 
     @IBAction func discoverButton(_ sender: Any) {
@@ -34,7 +31,10 @@ final class FromTignumCell: BaseDailyBriefCell {
         guard let model = viewModel else { return }
         skeletonManager.hide()
         baseHeaderView?.configure(title: (model.title ?? "").uppercased(), subtitle: model.subtitle)
+        ThemeText.dailyBriefFromTignumTitle.apply((model.subtitle ?? ""), to: baseHeaderView?.subtitleTextView)
         ThemeText.dailyBriefTitle.apply((model.title ?? "").uppercased(), to: baseHeaderView?.titleLabel)
         ThemeText.bespokeText.apply(model.text, to: fromTignumText)
+        headerHeightConstraint.constant = baseHeaderView?.calculateHeight(for: self.frame.size.width) ?? 0
+        baseHeaderView?.subtitleTextViewBottomConstraint.constant = 0
     }
 }
