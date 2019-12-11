a1a639744515a276b990e45ab7ef2d119669a2ec
Sanggeon Park <gunnih@gmail.com>
2019-12-11 10:47:52 +0100

    changed Guided Track CTA button position. removed Guided Track Cell Title changed Guided Track Cell Subtitle and Body text position.

--- QOT/Sources/GuidedTrackRowCell.swift
+++ QOT/Sources/GuidedTrackRowCell.swift
@@ -11,31 +11,34 @@ import qot_dal
 
 final class GuidedTrackRowCell: BaseDailyBriefCell {
 
-    @IBOutlet private weak var title: UILabel!
-    @IBOutlet private weak var subtitle: UILabel!
-    @IBOutlet private weak var content: UILabel!
+    @IBOutlet private weak var subtitle: UILabel?
+    @IBOutlet private weak var content: UILabel?
     @IBOutlet weak var dividerView: UIView!
-    @IBOutlet private weak var button: AnimatedButton!
+    @IBOutlet private weak var button: AnimatedButton?
     weak var delegate: DailyBriefViewControllerDelegate?
     private var appLink: QDMAppLink?
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        button.corner(radius: Layout.cornerRadius20, borderColor: .accent)
-        skeletonManager.addTitle(title)
-        skeletonManager.addSubtitle(subtitle)
-        skeletonManager.addSubtitle(content)
-        skeletonManager.addOtherView(button)
+        button?.corner(radius: Layout.cornerRadius20, borderColor: .accent)
+        if subtitle != nil {
+            skeletonManager.addSubtitle(subtitle!)
+        }
+        if content != nil {
+            skeletonManager.addSubtitle(content!)
+        }
+        if button != nil {
+            skeletonManager.addSubtitle(button!)
+        }
     }
 
     func configure(with: GuidedTrackViewModel?, _ hideDivider: Bool) {
         guard let model = with else { return }
         skeletonManager.hide()
-        title.text = model.bucketTitle
-        subtitle.text = model.levelTitle
-        content.text = model.content
+        subtitle?.text = model.levelTitle
+        content?.text = model.content
         self.appLink = model.appLink
-        button.setTitle(model.buttonText, for: .normal)
+        button?.setTitle(model.buttonText, for: .normal)
         dividerView.isHidden = hideDivider
     }
 
