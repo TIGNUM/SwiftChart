3c70a4632590ddd68bb0931c011442203156fbc6
Sanggeon Park <gunnih@gmail.com>
2020-02-12 16:39:21 +0100

    changed logic of scheduling Daily Check-In notification. From now on, we can individually handle notifications per weekday.

--- QOT/Sources/MyQotAdminSettings/MyQOTAdminLocalNotificationSettings/MyQotAdminLocalNotificationsInteractor.swift
+++ QOT/Sources/MyQotAdminSettings/MyQOTAdminLocalNotificationSettings/MyQotAdminLocalNotificationsInteractor.swift
@@ -68,7 +68,7 @@ extension MyQotAdminLocalNotificationsInteractor: MyQotAdminLocalNotificationsIn
                                                                soundName: "QotNotification.aiff",
                                                                link: link)
                     let trigger = UNCalendarNotificationTrigger(localTriggerDate: triggerDate)
-                    let identifier = QDMGuideItemNotification.notificationIdentifier(with: DAILY_CHECK_IN_NOTIFICATION_IDENTIFIER,
+                    let identifier = QDMGuideItemNotification.notificationIdentifier(with: DEBUG_LOCAL_NOTIFICATION_IDENTIFIER,
                                                                                     date: triggerDate,
                                                                                     link: link)
                     UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)) { (_) in
