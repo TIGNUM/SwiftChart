059ba7a2aed12f99f71e9151a3f419024005748e
Sanggeon Park <gunnih@gmail.com>
2020-03-12 15:46:30 +0100

    Fixed Issues QOT-2796: My Library // images and files shouldn't be shared in QOT QOT-2793: My Library // some apps don't recognize QOT to share links QOT-2794: My Library // sharing links from Youtube app QOT-2795: My Library // displaying thumbnails and titles

--- QOT/Sources/MyLibrary/ExternalLinkImporter/ExternalLinkImporter.swift
+++ QOT/Sources/MyLibrary/ExternalLinkImporter/ExternalLinkImporter.swift
@@ -20,11 +20,13 @@ final class ExternalLinkImporter {
 
     init() {
         // add Listener
-        NotificationCenter.default.addObserver(self, selector: #selector(onLogout(_:)), name: .userLogout, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(onLogout(_:)), name: .automaticLogout, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(load(_:)), name: .userLogin, object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(load(_:)),
+        NotificationCenter.default.addObserver(self, selector: #selector(onLogout(_:)),
+                                               name: .userLogout, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(onLogout(_:)),
+                                               name: .automaticLogout, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(load(_:)),
+                                               name: .userLogin, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(load(_:)),
                                                name: .UIApplicationDidBecomeActive, object: nil)
     }
 
@@ -38,12 +40,38 @@ final class ExternalLinkImporter {
 
     func importLink() {
         guard SessionService.main.getCurrentSession() != nil,
-            let externalLink: ShareExtentionData = ExtensionUserDefaults.object(for: .share, key: .saveLink),
-            let title = externalLink.title, let url = externalLink.url else {
+            let externalLink: ShareExtentionData = ExtensionUserDefaults.object(for: .share, key: .saveLink) else {
+                updateLinkTitleAndThumbnail()
                 return
         }
         ExtensionUserDefaults.removeObject(for: .share, key: .saveLink)
-        UserStorageService.main.addLink(title: title, url: url) { (storage, error) in
+        if let url = externalLink.url {
+            UserStorageService.main.addLink(title: "", url: url) { (_, _) in
+                self.updateLinkTitleAndThumbnail()
+            }
+
+        } else if externalLink.type == UserStorageType.NOTE.rawValue, let note = externalLink.description {
+            UserStorageService.main.addNote(note) { (_, _) in
+                self.updateLinkTitleAndThumbnail()
+            }
+        }
+    }
+
+    func updateLinkTitleAndThumbnail() {
+        UserStorageService.main.getUserStorages(for: .EXTERNAL_LINK) { (links, _, error) in
+            for link in links ?? [] where (link.title ?? "").isEmpty == true {
+                var link = link
+                let parser = OpenGraphMetaDataParser(with: URL(string: link.url ?? ""))
+                parser.parseMeta { (meta, error) in
+                    guard error == nil, meta != nil else {
+                        return
+                    }
+
+                    link.title = meta?.content(for: .title)
+                    link.note = meta?.content(for: .image)
+                    UserStorageService.main.updateUserStorage(link) { (_, _) in }
+                }
+            }
         }
     }
 }
---
+++ QOT/Sources/OpenGraphMetaDataParser.swift
@@ -0,0 +1,115 @@
+//
+//  OpenGraphMetaDataParser.swift
+//  QOT
+//
+//  Created by Sanggeon Park on 12.03.20.
+//  Copyright © 2020 Tignum. All rights reserved.
+//
+
+import Foundation
+
+typealias MetaDataResult = (OpenGraphMetaData?, Error?) -> Void
+
+struct OpenGraphMetaData {
+    enum property: String {
+        case title = "og:title"
+        case image = "og:image"
+        case url = "og:url"
+        case description = "og:description"
+        case locale = "og:locale"
+        case type = "og:type"
+    }
+
+    private let metaDataDictionary: [String: String]
+
+    init(_ meta: [String: String]) {
+        metaDataDictionary = meta
+    }
+
+    func content(for property: OpenGraphMetaData.property) -> String? {
+        return content(for: property.rawValue)
+    }
+
+    func content(for property: String) -> String? {
+        return metaDataDictionary[property]
+    }
+}
+
+final class OpenGraphMetaDataParser: NSObject, XMLParserDelegate {
+    private var url: URL?
+    private var completion: MetaDataResult?
+    private var dictionary = [String: String]()
+    private var currentElement = ""
+    private var imageURL: String?
+    private var title: String?
+    init(with url: URL?) {
+        self.url = url
+    }
+
+    func parseMeta(_ completion: @escaping MetaDataResult) {
+        self.completion = completion
+        guard let url = self.url else {
+            self.completion?(nil, nil)
+            self.completion = nil
+            return
+        }
+        dictionary.removeAll()
+        currentElement = ""
+        imageURL = nil
+        title = nil
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let data = try Data(contentsOf: url)
+                let string = String(data: data, encoding: .utf8) ?? String(data: data, encoding: .ascii) ?? ""
+                let regex = try NSRegularExpression(pattern: "<meta.*>|<title.*.title>|<link.*>")
+                _ = regex.matches(in: string, range: NSRange(string.startIndex..., in: string)).compactMap {
+                    let metaString = String(string[Range($0.range, in: string)!])
+                    let xmlParser = XMLParser(data: metaString.data(using: .utf8) ?? Data())
+                    xmlParser.delegate = self
+                    xmlParser.parse()
+                }
+
+                DispatchQueue.main.async {
+                    if self.dictionary[OpenGraphMetaData.property.image.rawValue] == nil,
+                        let imageURL = self.imageURL {
+                        self.dictionary[OpenGraphMetaData.property.image.rawValue] = imageURL
+                    }
+                    if self.dictionary[OpenGraphMetaData.property.title.rawValue] == nil,
+                        let title = self.title {
+                        self.dictionary[OpenGraphMetaData.property.title.rawValue] = title
+                    }
+                    self.completion?(OpenGraphMetaData(self.dictionary), nil)
+                    self.completion = nil
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.completion?(nil, error)
+                    self.completion = nil
+                }
+            }
+        }
+    }
+
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?,
+                qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        if elementName == "meta", let poperty = attributeDict["property"] {
+            dictionary[poperty] = attributeDict["content"]
+        } else if self.imageURL == nil, elementName == "link",
+            let rel = attributeDict["rel"], rel.lowercased().contains("icon"),
+            let href = attributeDict["href"] {
+            let scheme = self.url?.scheme ?? "http"
+            if href.hasPrefix(scheme) {
+                self.imageURL = href
+            } else {
+                self.imageURL = "\(scheme)://\(self.url?.host ?? "")\(href)"
+            }
+        }
+    }
+
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if currentElement.lowercased() == "title" {
+            title = (title ?? "") + string
+        }
+    }
+}
