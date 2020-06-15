e5203a27786aafcc47009a60f1d808bbcda5a82d
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2020-06-15 15:24:37 +0200

    setting height

--- QOT/Sources/ExpertThoughtsTableViewCell.swift
+++ QOT/Sources/ExpertThoughtsTableViewCell.swift
@@ -15,11 +15,12 @@ final class ExpertThoughtsTableViewCell: BaseDailyBriefCell {
     @IBOutlet private weak var descriptionLabel: UILabel!
     @IBOutlet private weak var headerView: UIView!
     @IBOutlet private weak var expertNameLabel: UILabel!
-
     private var mediaURL: URL?
     private var duration: Double?
     private var remoteID: Int?
     private var audioTitle: String?
+    @IBOutlet weak var audioButtonHeightConstraint: NSLayoutConstraint!
+
     weak var delegate: DailyBriefViewControllerDelegate?
 
     override func awakeFromNib() {
@@ -34,21 +35,24 @@ final class ExpertThoughtsTableViewCell: BaseDailyBriefCell {
     func configure(with viewModel: ExpertThoughtsCellViewModel?) {
         guard let model = viewModel else { return }
         skeletonManager.hide()
+        audioButton.isHidden = model.format != .audio
         baseHeaderView?.configure(title: (model.title ?? "").uppercased(),
                                   subtitle: "")
         baseHeaderView?.subtitleTextViewBottomConstraint.constant = 0
         ThemeText.dailyBriefTitle.apply((model.title ?? "").uppercased(), to: baseHeaderView?.titleLabel)
         ThemeText.dailyBriefSubtitle.apply(model.description ?? "", to: descriptionLabel)
         ThemeText.dailyBriefSubtitle.apply(model.name ?? "", to: expertNameLabel)
-
         descriptionLabel.isHidden = model.description == nil
         duration = model.audioDuration
         remoteID = model.remoteID
         audioTitle = model.audioTitle
         mediaURL = model.audioLink
-        audioButton.isHidden = model.format != .audio
+        if audioButton.isHidden == true {
+             audioButtonHeightConstraint.constant = 0
+        } else {
         let mediaDescription = String(format: "%02i:%02i", Int(duration ?? 0) / 60 % 60, Int(duration ?? 0) % 60)
         audioButton.setTitle(mediaDescription, for: .normal)
+        }
     }
 
     @IBAction func audioAction(_ sender: Any) {
