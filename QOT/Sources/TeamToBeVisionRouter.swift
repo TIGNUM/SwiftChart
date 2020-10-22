449fc09b3d77a481ed15744f5bec7323cb2459da
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2020-10-22 20:00:39 +0200

    removed comments

--- QOT/Sources/TeamToBeVisionRouter.swift
+++ QOT/Sources/TeamToBeVisionRouter.swift
@@ -7,77 +7,7 @@
 //
 
 import UIKit
-//import qot_dal
+
 
 final class TeamToBeVisionRouter: BaseRouter {}
-//
-//final class TeamToBeVisionRouter {
-//
-//    // MARK: - Properties
-//    private weak var viewController: TeamToBeVisionViewController?
-//
-//    // MARK: - Init
-//    init(viewController: TeamToBeVisionViewController?) {
-//        self.viewController = viewController
-//    }
-//}
-//
-//// MARK: - TeamToBeVisionRouterInterface
-//extension TeamToBeVisionRouter: TeamToBeVisionRouterInterface {
-//
-//    func dismiss() {
-//        viewController?.dismiss(animated: true, completion: nil)
-//    }
-//
-//    func showEditVision(title: String, vision: String, isFromNullState: Bool, team: QDMTeam?) {
-//        guard
-//            let controller = R.storyboard.myToBeVision.myVisionEditDetailsViewController(),
-//            let visionController = self.viewController else { return }
-//        MyVisionEditDetailsConfigurator.configure(viewController: controller,
-//                                                  title: title,
-//                                                  vision: vision,
-//                                                  isFromNullState: isFromNullState,
-//                                                  team: team)
-//        visionController.present(controller, animated: true, completion: nil)
-//    }
-//
-//    func showAlert(type: AlertType, handler: (() -> Void)?, handlerDestructive: (() -> Void)?) {
-//        viewController?.showAlert(type: type, handler: handler, handlerDestructive: handlerDestructive)
-//    }
-//
-//    func showViewController(viewController: UIViewController, completion: (() -> Void)?) {
-//        self.viewController?.present(viewController, animated: true, completion: completion)
-//    }
-//
-//    func showRatingExplanation(team: QDMTeam?) {
-//        let controller = R.storyboard.visionRatingExplanation.visionRatingExplanationViewController()
-//        if let controller = controller {
-//            let type: Explanation.Types = (team?.thisUserIsOwner == true) ? .ratingOwner : .ratingUser
-//            showRating(team, type, controller)
-//        }
-//    }
-//
-//    func showTbvPollEXplanation(team: QDMTeam?) {
-//        let controller = R.storyboard.visionRatingExplanation.visionRatingExplanationViewController()
-//        if let controller = controller {
-//            let type: Explanation.Types = (team?.thisUserIsOwner == true) ? .tbvPollOwner : .tbvPollUser
-//            showRating(team, type, controller)
-//        }
-//    }
-//
-//    func showAdminOptions(team: QDMTeam?, remainingDays: Int) {
-//        guard let optionsViewController = R.storyboard.teamToBeVisionOptions.teamToBeVisionOptionsViewController() else { return }
-//        let configurator = TeamToBeVisionOptionsConfigurator.make(type: .rating, remainingDays: remainingDays, team: team)
-//        configurator(optionsViewController)
-//        viewController?.present(optionsViewController, animated: true)
-//    }
-//}
-//
-//// MARK: - Private
-//private extension TeamToBeVisionRouter {
-//    func showRating(_ team: QDMTeam?, _ type: Explanation.Types, _ controller: VisionRatingExplanationViewController) {
-//        let configurator = VisionRatingExplanationConfigurator.make(team: team, type: type)
-//        configurator(controller)
-//        viewController?.present(controller, animated: true)
-//    }
-//}
+
