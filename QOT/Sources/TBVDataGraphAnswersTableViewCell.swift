da3ae823ca43c26f97a9950216fdfbb81db54e10
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2020-11-17 13:33:50 +0100

    cleaned

--- QOT/Sources/TBVDataGraphAnswersTableViewCell.swift
+++ QOT/Sources/TBVDataGraphAnswersTableViewCell.swift
@@ -50,7 +50,8 @@ private extension TBVDataGraphAnswersTableViewCell {
 
     func setupTheme(_ sentence: QDMToBeVisionSentence, isTeam: Bool) {
         ThemeText.tbvTrackerAnswerTeam.apply(sentence.text, to: answerLabel)
-        ThemeText.tbvTrackerRating.apply(AppTextService.get(isTeam ? .my_x_team_vision_tracker_average_rating : .my_qot_my_tbv_tbv_tracker_data_section_my_tbv_label_last_ratings),
+        ThemeText.tbvTrackerRating.apply(AppTextService.get(isTeam ? .my_x_team_vision_tracker_average_rating :
+                                                                .my_qot_my_tbv_tbv_tracker_data_section_my_tbv_label_last_ratings),
             to: lastRatingLabel)
     }
 
