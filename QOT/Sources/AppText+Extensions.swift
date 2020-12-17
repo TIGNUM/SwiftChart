ce05d9e7716da91ee608a162691d89479bf3a380
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2020-12-17 15:47:00 +0100

    added apptext

--- QOT/Sources/AppText+Extensions.swift
+++ QOT/Sources/AppText+Extensions.swift
@@ -851,6 +851,7 @@ public extension AppTextKey {
     static let my_qot_my_tbv_alert_update_create = AppTextKey("my_qot.my_tbv.alert.update.create")
     static let my_qot_my_tbv_empty_subtitle_vision = AppTextKey("my_qot.my_tbv.empty.subtitle_vision")
     static let myx_team_tbv_empty_subtitle_vision = AppTextKey("myx.team_tbv.empty_subtitle_vision")
+    static let myx_team_tbv_empty_subtitle_vision_new = AppTextKey("myx.team_tbv.empty_subtitle_vision_new")
     static let my_qot_my_tbv_null_state_title = AppTextKey("my_qot.my_tbv.null_state.title")
     static let myx_team_tbv_null_state_title = AppTextKey("my_x.team_tbv_null_state.title")
     static let myx_team_tbv_null_state_subtitle = AppTextKey("my_x.team_tbv_null_state.subtitle")
