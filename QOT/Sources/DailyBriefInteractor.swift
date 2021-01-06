cfd0e066c2cc457ee02ede926dcffa1dd86b634c
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2021-01-06 11:17:50 +0100

    added quotes!

--- QOT/Sources/DailyBriefInteractor.swift
+++ QOT/Sources/DailyBriefInteractor.swift
@@ -1184,7 +1184,7 @@ extension DailyBriefInteractor {
         guard vision != nil else { return teamVisionList }
         let imageURL = vision?.profileImageResource?.remoteURLString == nil ? teamVisionBucket.bucketImages?.first?.mediaUrl : vision?.profileImageResource?.remoteURLString
         let team = teamVisionBucket.myTeams?.filter { $0.qotId == vision?.teamQotId }.first
-        let visionSentence = vision?.sentences.first?.sentence
+        let visionSentence = "\"" + (vision?.sentences.first?.sentence ?? "") + "\""
         let title = AppTextService.get(.my_x_team_tbv_new_section_header_title).replacingOccurrences(of: "{$TEAM_NAME}", with: team?.name ?? "").uppercased()
         let suggestion = DailyBriefAtMyBestWorker().storedTeamVisionText(collections.randomElement()?.contentItems.first?.valueText ?? " ")
         let model = TeamVisionSuggestionModel(title: title, team: team, tbvSentence: visionSentence, adviceText: suggestion, imageURL: imageURL, domainModel: teamVisionBucket)
