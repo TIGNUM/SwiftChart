ce87debbe28aa9fe68b500466da240d92e67db7c
Simu Voicu-Mircea <voicu.simu@hotmail.com>
2021-01-08 21:39:55 +0200

    Adjuste Solve section UI to match new design

--- QOT/Sources/SolveStrategyTableViewCell.swift
+++ QOT/Sources/SolveStrategyTableViewCell.swift
@@ -15,6 +15,7 @@ final class SolveStrategyTableViewCell: DTResultBaseTableViewCell, Dequeueable {
     @IBOutlet private weak var headerLabel: UILabel!
     @IBOutlet private weak var strategyTitleLabel: UILabel!
     @IBOutlet private weak var minsToReadLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet private weak var topSeperatorView: UIView!
     @IBOutlet private weak var constraintHeaderHeight: NSLayoutConstraint!
 }
@@ -27,6 +28,8 @@ extension SolveStrategyTableViewCell {
         ThemeText.resultHeader2.apply(title, to: strategyTitleLabel)
         ThemeText.resultDate.apply(minsToRead, to: minsToReadLabel)
         ThemeText.resultList.apply(headerTitle, to: headerLabel)
+        iconImageView.image = R.image.ic_read()
+        ThemeTint.darkGrey.apply(iconImageView)
 
         let designerTopBottomMargin: CGFloat = 24 + 28
         constraintHeaderHeight.constant = hasHeader ? designerTopBottomMargin + headerLabel.bounds.height : 24
