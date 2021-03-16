a5a119e0150e0b1389b5aa534f18f7aed2df9336
karmic <karbemichael@gmail.com>
2021-03-16 18:10:47 +0100

    Testing now also the other/false case

--- TignumXTests/FeatureFlagTests.swift
+++ TignumXTests/FeatureFlagTests.swift
@@ -11,12 +11,16 @@ import qot_dal
 @testable import TIGNUM_X
 
 class FeatureFlagTests: XCTestCase {
-    func testActivatedFeatureEditableNotification() {
+    func testEditableNotification() {
         let environment = Environment()
         switch environment.current {
         case .DEV,
-             .INT: XCTAssertTrue(Feature.Flag.editableNotifications.isOn)
-        case .PUB: XCTAssertFalse(Feature.Flag.editableNotifications.isOn)
+             .INT:
+            XCTAssertTrue(Feature.Flag.editableNotifications.isOn == true)
+            XCTAssertFalse(Feature.Flag.editableNotifications.isOn == false)
+        case .PUB:
+            XCTAssertFalse(Feature.Flag.editableNotifications.isOn == true)
+            XCTAssertTrue(Feature.Flag.editableNotifications.isOn == false)
         }
      }
 }
